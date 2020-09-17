54de8f100fa770be2ec1773d7cf30ec384b55459
Jeff <heffayXD@gmail.com>
2020-09-16 19:47:43 -0600

    Implemented Redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import 'regenerator-runtime'
 import React from 'react'
 import { render } from 'react-dom'
-
+import { Provider } from 'react-redux'
 import './styles/index.scss'
 
+import { configureStore } from './store'
 import App from './App'
 
-render((<App />), document.getElementById('root'))
+const store = configureStore()
+
+render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'))
