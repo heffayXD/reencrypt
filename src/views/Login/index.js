a60449a305360b55b5a58b78ae4730f42fefc6b2
Jeff <heffayXD@gmail.com>
2020-09-16 19:28:11 -0600

    Added buttons for login

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.scss'
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({ username: '', password: '' })
+
+  const handleSubmit = async e => {
+    try {
+      e.preventDefault()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
   return (
     <div id='login'>
-      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='input-container'>
+          <input
+            name='username'
+            type='text'
+            placeholder='Username'
+            value={credentials.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className='input-container'>
+          <input
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={credentials.password}
+            onChange={handleChange}
+          />
+        </div>
+        <input type='submit' value='Submit' />
+      </form>
     </div>
   )
 }
